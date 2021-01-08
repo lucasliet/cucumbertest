17b795643a8e9d6d967641f6e1a1ae8b4758f73e
lucasliet <lucasouliveira@gmail.com>
2021-01-08 09:41:57 -0300

    init

---
+++ tests/features/step_definitions/primeiro_cenario.rb
@@ -0,0 +1,8 @@
+Quando('eu somo {int} + {int}') do |valor1, valor2|
+  @soma = valor1 + valor2
+end
+
+Então('o resultado tem que ser {int}') do |resultado|
+  expect(@soma).to eq resultado
+  log("[Soma #{@soma}] = [Resultado #{resultado}] : #{@soma == resultado}")
+end
